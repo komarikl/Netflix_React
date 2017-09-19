72f0c7814e876ecb74bc3d1b1912e4f1bd846fca
Lada Ilyina <lada_ilyina@epam.com>
2017-09-19 03:54:29 +0300

    layout

---
+++ src-js/components/Logo.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const Logo = () => (
+    <span className="logo">netflixroulette</span>
+);
---
+++ src-js/components/movie/ReleaseYear.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const ReleaseYear = (props) => (
+    <span>{props.year}</span>
+);
